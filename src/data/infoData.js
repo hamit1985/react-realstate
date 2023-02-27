7f1f56c5d8eac8c56825fcaaa2cc45c8176b518e
hamit1985 <111700185+hamit1985@users.noreply.github.com>
2023-02-27 05:54:55 +0300

    finished

---
+++ src/data/infoData.js
@@ -0,0 +1,22 @@
+import imageOne from '../images/house-8.jpg'
+import imageTwo from '../images/house-9.jpg'
+
+export const InfoData = {
+    heading: ' Explore our beautiful homes',
+    paragraphOne: " lasdlkjflakjdklsasalkfa sdlfkjsflskd sdlksjfslfkj sdfs sdsfssf gkjjkhhjkhhkhk",
+    paragraphTwo: "aslakjaslasaslakasjfalkfja slksjflfksj sdlfksjflsdk slkdsfj", 
+    buttonLabel: 'view Homes',
+    image : imageOne,
+    reverse: false,
+    delay: 100
+}
+
+export const InfoDataTwo = {
+    heading: ' Modern Designs',
+    paragraphOne: " lasdlkjflakjdklsasalkfa sdlfkjsflskd sdlksjfslfkj sdfs sdsfssf gkjjkhhjkhhkhk",
+    paragraphTwo: "aslakjaslasaslakasjfalkfja slksjflfksj sdlfksjflsdk slkdsfj", 
+    buttonLabel: 'view Homes',
+    image : imageTwo,
+    reverse: true,
+    delay: 300
+}
